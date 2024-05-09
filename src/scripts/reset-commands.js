2d256a881a9daf7a1c9ba2e9a3e3f75fbb6d48b5
Saahil <saahilattud@gmail.com>
2024-05-09 00:55:48 +0000

    scripts

---
+++ src/scripts/reset-commands.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: require('path').dirname(__dirname) })
+const { REST, Routes } = require('discord.js');
+const { CLIENT_ID: clientId, GUILD_ID: guildId, DISCORD_TOKEN: token } = process.env
+const commands = [];
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
